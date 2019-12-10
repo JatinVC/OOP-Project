4de4c89c724337c21f29bd3279204b34f0ec3514
Jatin <jchandwaney@gmail.com>
2019-12-11 07:12:40 +0800

    Bat files for compilation and running

---
+++ src/g18_c.bat
@@ -0,0 +1,16 @@
+@echo off
+javac AboutUs.java
+javac AdminPage.java
+javac CreateUsers.java
+javac DeleteLoginRecord.java
+javac DeleteUsers.java
+javac G18m1Record.java
+javac G18m1WONGTestMain.java
+javac G18M4APPOINTMENT.java
+javac G18M4MAIN.java
+javac G18M4TESTMAIN.java
+javac Login.java
+javac LoginRecord.java
+javac LoginTest.java
+javac User.java
+javac Userpage.java
---
+++ src/g18_r.bat
@@ -0,0 +1,3 @@
+@echo off
+java LoginTest
+pause
